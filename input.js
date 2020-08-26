bb81fa0ba26ba0bb2551f7c3a66d9c416a273189
Kevin Lee <kevin.skyulee@gmail.com>
2020-08-26 20:15:20 +0000

    added wasd movement functionality to input

--- input.js
+++ input.js
@@ -1,16 +1,31 @@
-const setupInput = () => {
+// STores the active TCP connection object
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   
+
   return stdin;
 };
 
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === '\u0077') {
+    connection.write('Move: up');
+  } else if (key === '\u0064') {
+    connection.write('Move: right');
+  } else if (key === '\u0073') {
+    connection.write('Move: down');
+  } else if (key === '\u0061') {
+    connection.write('Move: left');
   }
 };
 
-module.exports = setupInput;
+setupInput().on('data', handleUserInput);
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,6 +2,7 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input');
 
-setupInput().on('data', handleUserInput);
 console.log('Connecting...');
-connect();
+const connection = connect();
+
+setupInput(connection); //We're passing in the conn object into the input so we can use it in our input function
