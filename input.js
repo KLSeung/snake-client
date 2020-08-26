037187b749745581cac47a37f02d5e3054af4940
Kevin Lee <kevin.skyulee@gmail.com>
2020-08-26 19:59:26 +0000

    added input function as a separate module

---
+++ input.js
@@ -0,0 +1,16 @@
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
